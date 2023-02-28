63b4feaa4061bf151036ce9248dd994b948e1698
phungthanhtu <thanhtucqtuit1952@gmail.com>
2023-02-28 09:21:14 +0700

    windows batchscript to mkdir

--- startup.bat
+++ startup.bat
@@ -1,3 +1,4 @@
-# docker build -t isolate-engine .
-# docker run --privileged -p 8000:8000 --name isolate-container -v $(PWD)/example:/usr/local/etc/isolate/example/ -v $(PWD)/server:/server isolate-engine
+if not exist \example mkdir -p \example 
+if not exist \cg\memory\ mkdir -p \cg\memory\ 
+if not exist \cg\cpuacct\ mkdir -p \cg\cpuacct\ 
 docker-compose -f ./docker-compose.yml up -d --build
