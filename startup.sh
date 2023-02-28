c7a495f18e2282fa62a88f9fad6c689c0241b085
phungthanhtu <thanhtucqtuit1952@gmail.com>
2023-02-28 09:11:33 +0700

    create cgroup on startup

--- startup.sh
+++ startup.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
 # docker build -t isolate-engine .
 # docker run --privileged -p 8000:8000 --name isolate-container -v $(PWD)/example:/usr/local/etc/isolate/example/ -v $(PWD)/server:/server isolate-engine
+mkdir -p example
+mkdir -p cg/memory
+mkdir -p cg/cpuacct
 docker-compose -f ./docker-compose.yml up -d --build
