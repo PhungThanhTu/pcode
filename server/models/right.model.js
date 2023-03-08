284395a7391aa2b0c4230793ee2920a14ceb2dec
phungthanhtu <thanhtucqtuit1952@gmail.com>
2023-03-08 21:00:45 +0700

    create invitation on course creation

--- server/models/right.model.js
+++ server/models/right.model.js
@@ -1,13 +1,13 @@
 const sql = require('mssql');
 const sqlConfig = require('../configs/mssqlConfig');
 
-exports.grantRoleToCourseSql = async (userId, courseId, plpRole) => {
+exports.grantRoleToCourseSql = async (userId, courseId, plpRoleId) => {
     const pool = await sql.connect(sqlConfig);
 
     await pool.request()
         .input('userId', sql.UniqueIdentifier, userId)
         .input('courseId',sql.UniqueIdentifier, courseId)
-        .input('plpRole',sql.VarChar(10), plpRole)
+        .input('plpRole',sql.Int, plpRoleId)
         .execute('GrantUserRoleToCourse');
         
     await pool.close();
