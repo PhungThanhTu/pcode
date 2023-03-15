f1aaee58a6530d9b9f2845a06cfc16fa5904a71d
phungthanhtu <thanhtucqtuit1952@gmail.com>
2023-03-15 20:35:01 +0700

    update procedure

--- database/02_course.sql
+++ database/02_course.sql
@@ -2,10 +2,20 @@
 ---- PROCEDURES 
 create procedure CreateCourse
     @id UNIQUEIDENTIFIER,
-    @title nvarchar(max)
+    @title nvarchar(max),
+    @courseSubject nvarchar(max) = 'General',
+    @courseTheme nvarchar(max) = NULL
     AS
     INSERT INTO [dbo].[Course]
     (id, title)
     VALUES
     (@id, @title)
 GO
+
+create table Course
+(
+    id UNIQUEIDENTIFIER PRIMARY KEY,
+    title nvarchar(max),
+    courseSubject nvarchar(max),
+    courseTheme nvarchar(max)
+);
