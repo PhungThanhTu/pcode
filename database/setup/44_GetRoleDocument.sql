04ce2d6516a510668bd96f13966aac87487f1082
phungthanhtu <19522455@gm.uit.edu.vn>
2023-04-22 10:17:50 +0700

    upate change get role procs

--- database/setup/44_GetRoleDocument.sql
+++ database/setup/44_GetRoleDocument.sql
@@ -2,7 +2,7 @@ create procedure GetRoleOnDocument
     @DocumentId UNIQUEIDENTIFIER,
     @UserId UNIQUEIDENTIFIER
     as
-    select * from [dbo].[Document] d
+    select PlpRole as [Role] from [dbo].[Document] d
         join [dbo].[DocumentCourse] dc on d.Id = dc.DocumentId
         join [dbo].[PlpCourseAuthorization] ca on dc.CourseId = ca.CourseId
         where d.Id = @DocumentId
