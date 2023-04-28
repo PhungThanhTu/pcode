b8c0a73c6d0a055de4d0b258d9752c28cf620b45
phungthanhtu <19522455@gm.uit.edu.vn>
2023-04-28 23:33:52 +0700

    update UpdateExerciseByDocumentId procedure to ensure keep existing property if there is no corresponding param

--- database/setup/58_UpdateExerciseByDocumentId.sql
+++ database/setup/58_UpdateExerciseByDocumentId.sql
@@ -1,22 +1,22 @@
 create procedure UpdateExerciseByDocumentId
     @DocumentId UNIQUEIDENTIFIER,
-    @RuntimeLimit int,
-    @MemoryLimit int,
-    @ScoreWeight int,
-    @HaveDeadline bit,
-    @Deadline datetime2,
-    @StrictDeadline bit,
-    @ManualPercentage float
+    @RuntimeLimit int = NULL,
+    @MemoryLimit int = NULL,
+    @ScoreWeight int = NULL,
+    @HaveDeadline bit = NULL,
+    @Deadline datetime2 = NULL,
+    @StrictDeadline bit = NULL,
+    @ManualPercentage float = NULL
     AS
         update [dbo].[Exercise]
         SET
-            RuntimeLimit = @RuntimeLimit,
-            MemoryLimit = @MemoryLimit,
-            ScoreWeight = @ScoreWeight,
-            HaveDeadline = @HaveDeadline,
-            Deadline = @Deadline,
-            StrictDeadline = @StrictDeadline,
-            ManualPercentage = @ManualPercentage
+            RuntimeLimit = COALESCE(@RuntimeLimit, RuntimeLimit),
+            MemoryLimit = COALESCE(@MemoryLimit, MemoryLimit),
+            ScoreWeight = COALESCE(@ScoreWeight, ScoreWeight), 
+            HaveDeadline = COALESCE(@HaveDeadline, HaveDeadline),
+            Deadline = COALESCE(@Deadline, Deadline),
+            StrictDeadline = COALESCE(@StrictDeadline, StrictDeadline),
+            ManualPercentage = COALESCE(@ManualPercentage, ManualPercentage)
         FROM [dbo].[Exercise] E
         JOIN [dbo].[DocumentExercise] DE
         ON E.Id = DE.ExerciseId
