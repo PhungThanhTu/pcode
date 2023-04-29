e775168499551762598fe9bd06079ea3e23dba8a
phungthanhtu <19522455@gm.uit.edu.vn>
2023-04-30 01:57:25 +0700

    get exercise store procedure (untested)

---
+++ database/setup/62_GetSampleSourceCodeByDocumentId.sql
@@ -0,0 +1,15 @@
+create procedure GetSampleSourceCodeByDocumentId
+    @DocumentId UNIQUEIDENTIFIER,
+    @ProgrammingLanguageId INT
+    as
+        select
+            programmingLanguageId,
+            sourceCode
+        from [dbo].[SampleSourceCode] SSC
+        join [dbo].[Exercise] E
+            on SSC.exerciseId = E.Id
+        join [dbo].[DocumentExercise] DE
+            on E.Id = DE.ExerciseId
+        where 
+            DE.DocumentId = @DocumentId
+            and SSC.programmingLanguageId = @ProgrammingLanguageId
