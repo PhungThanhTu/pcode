a67c033b5507a284f55dbb3afd1ce7ea26909b4a
phungthanhtu <19522455@gm.uit.edu.vn>
2023-05-17 00:06:15 +0700

    Trigger for on SubmissionUpdate

---
+++ database/setup/96_Trigger_OnSubmissionUpdate.sql
@@ -0,0 +1,56 @@
+CREATE TRIGGER [dbo].[OnSubmissionUpdate]
+ON [dbo].[Submission]
+AFTER INSERT, DELETE
+AS
+BEGIN
+    declare @Choice BIT
+    select @Choice = Choice from inserted
+    if @Choice = 1
+        BEGIN
+            declare @SubmissionId UNIQUEIDENTIFIER
+            select @SubmissionId = Id from inserted
+            declare @AutomatedScore FLOAT
+            select @AutomatedScore = AutomatedScore from inserted
+            declare @ManualScore FLOAT
+            select @ManualScore = ManualScore from inserted
+            declare @ExerciseId UNIQUEIDENTIFIER
+            select @ExerciseId = ExerciseId from inserted
+            
+            declare @ManualPercentage float
+            select @ManualPercentage = ManualPercentage from [dbo].[Exercise]
+                where Id = @ExerciseId
+            declare @DocumentId UNIQUEIDENTIFIER
+
+            declare @UserId UNIQUEIDENTIFIER
+            select @UserId = UserId from [dbo].[SubmissionUser]
+                where SubmissionId = @SubmissionId
+
+            declare @Score float
+            SET @Score = @ManualScore * @ManualPercentage + @AutomatedScore * (1 - @ManualPercentage)
+
+            select @DocumentId = DocumentId from [dbo].[DocumentExercise]
+                where ExerciseId = @ExerciseId
+            MERGE [dbo].[DocumentScore] as DestinationTable
+            USING(
+                VALUES
+                (
+                    @UserId,
+                    @DocumentId,
+                    @Score
+                )
+            ) as UpsertingData
+            (
+                UserId,
+                DocumentId,
+                Score
+            )
+            ON DestinationTable.UserId = UpsertingData.UserId
+            AND DestinationTable.DocumentId = UpsertingData.DocumentId
+            WHEN NOT MATCHED THEN
+                INSERT (UserId, DocumentId, Score)
+                VALUES (@UserId, @DocumentId, @Score)
+            WHEN MATCHED THEN
+                UPDATE SET
+                    DestinationTable.Score = UpsertingData.Score;
+        END
+END
