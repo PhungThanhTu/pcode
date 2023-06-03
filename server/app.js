ce5455e7cfa292e15aed83bd688eb34d4fc97dd0
phungthanhtu <19522455@gm.uit.edu.vn>
2023-06-03 10:49:07 +0700

    camelize body

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ var courseRouter = require('./routes/course');
 var documentRouter = require('./routes/document');
 var mediaRouter = require('./routes/media');
 var publicRouter = require('./routes/public');
+const { camelizeBody } = require('./middlewares/common.middleware');
 
 var app = express();
 
@@ -21,12 +22,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(camelizeBody);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-
 router.use('/', indexRouter);
 router.use('/auth', authRouter);
 router.use('/profile', profileRouter);
@@ -35,7 +36,9 @@ router.use('/document', documentRouter);
 router.use('/media', mediaRouter);
 router.use('/public', publicRouter);
 
+
 app.use('/api',router);
+
 
 
 
---
+++ server/utils/string.utils.js
@@ -0,0 +1,36 @@
+module.exports.nameToField = (name, upper) => {
+
+	let tmp = name.trim().split(' ');
+
+	if (upper)
+		tmp[0] = tmp[0].charAt(0).toUpperCase() + tmp[0].substring(1)
+	else
+		tmp[0] = tmp[0].toLowerCase();
+
+	return tmp.join('');
+};
+
+module.exports.toCamel = function (o) {
+    var newO, origKey, newKey, value
+    if (o instanceof Array) {
+      return o.map(function(value) {
+          if (typeof value === "object") {
+            value = toCamel(value)
+          }
+          return value
+      })
+    } else {
+      newO = {}
+      for (origKey in o) {
+        if (o.hasOwnProperty(origKey)) {
+          newKey = (origKey.charAt(0).toLowerCase() + origKey.slice(1) || origKey).toString()
+          value = o[origKey]
+          if (value instanceof Array || (value !== null && value.constructor === Object)) {
+            value = toCamel(value)
+          }
+          newO[newKey] = value
+        }
+      }
+    }
+    return newO
+  }
