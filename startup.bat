b3ea190a38027e0ba21fd18a94e841da178a779d
phungthanhtu <thanhtucqtuit1952@gmail.com>
2023-07-15 10:59:55 +0700

    edit Windows setup and teardown script and add build scripts for windows

--- startup.bat
+++ startup.bat
@@ -1,5 +1,5 @@
-if not exist \example mkdir -p \example 
-if not exist \cg\memory\ mkdir -p \cg\memory\ 
-if not exist \cg\cpuacct\ mkdir -p \cg\cpuacct\ 
+if not exist \example mkdir \example 
+if not exist \cg\memory\ mkdir \cg\memory\ 
+if not exist \cg\cpuacct\ mkdir \cg\cpuacct\ 
 docker-compose -f ./docker/dev/docker-compose.yml up -d --build
 docker exec -it mssql node setup.js
